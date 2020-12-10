fbd3fa2411d8e47ebb5a50027d874f89188afa9b
VaidehiPrabhune <prabhunevaidehi13@gmail.com>
2020-12-10 21:40:56 +0530

    initial commit

---
+++ CAssignments/Debug/Prg1/main.d
@@ -0,0 +1,1 @@
+Prg1/main.o: ../Prg1/main.c
---
+++ CAssignments/Prg1/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ *
+ *  Created on: 15-Feb-2020
+ *      Author: sunbeam
+ */
+#include<stdio.h>
+int main(void)
+{
+	printf("hello");
+	return 0;
+}
